fee3e7c40da04a78b9ceb456beb87b64e6b7c87f
Mukhil <mukhil1140@gmail.com>
2024-08-14 18:24:20 +0000

    Refer env variables in settings.py

--- compsocsite/compsocsite/settings.py
+++ compsocsite/compsocsite/settings.py
@@ -25,11 +25,12 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = eval(env('DEBUG'))
 
+# SECURITY WARNING: allow only specific domains to access the site
 ALLOWED_HOSTS = ['*']
 
 
